e3aa73f052e2581ae7bfb7deb4c66def46edb476
Chris <32886763+Chris300127@users.noreply.github.com>
2019-03-08 10:48:52 -0800

    update

--- Lab5/Lab5Route.c
+++ Lab5/Lab5Route.c
@@ -2,13 +2,17 @@
 Lab5Route.c
 Created by: Chris Nelson on Friday, March 1, 2019
 
-Compile by: gcc -o lab5 Lab5Route -lpthread -std=C99
-Run by: ./lab5 {machine ID (0-3)} {num of machines} machines
-ex: ./lab5 0 4 machines
+Compile by: gcc -o lab5 Lab5Route.c -lpthread -std=C99
+Run by: ./lab5 {machine ID (0-3)} {num of machines} {cost file} {machines file}
+ex: ./lab5 0 4 cost machines
+
+1)router ID = argv[1]
+2)nUm of nodes = argv[2]
+3)cost = argv[3]
+4)machine = argv[4]
 */
 
-#define numNodes 5;
-
+#define numNodes 4
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,12 +21,15 @@ ex: ./lab5 0 4 machines
 #include <stdlib.h>
 
 /* define Machine struct */
-struct 
+typedef struct{
+    char name[50];
+    char ip[50];
+    int port;
+}Machine;
 
 
 /* Global Variable */
-int n;
-int* matrix;
+int cost_matrix[numNodes][numNodes];
 Machine linux_machines[numNodes];
 int sock;
 pthread_mutex_t lock;
@@ -41,9 +48,31 @@ void* recieveInfo(void* b){
 
 /* print out table */
 void printTable(void){
-
+    int i;
+    for(i=0; i<4; ++i){
+        printf("%d %d %d %d\n", cost_matrix[i][0], cost_matrix[i][1], cost_matrix[i][2], cost_matrix[i][3]);
+    }
 }
 /* Main Function */
 int main(int argc, char *argv[]){
+    int i;
+    /* Fill cost matrix */{
+    FILE* fp = fopen(argv[3], "r"); //open cost file for reading
+
+    for(i=0; i<argv[2]; ++i){
+        fscanf(fp, "%d %d %d %d", &cost_matrix[i][0], &cost_matrix[i][1], &cost_matrix[i][2], &cost_matrix[i][3]);
+    }
+    fclose(fp);
+    }
+    printTable();
+
+    /* Fill Machine structs */{
+    fp = fopen(argv[4], "r");   //open machines file for reading
+    for(i=0; i<argv[2]; ++i){
+        fscanf(fp, "%s %s %d", linux_machines[i].)
+    }
+    }
+
+
 
 }
