473fa5e08e1a8a73a5eceba995cffe9cf53f9615
Chris <32886763+Chris300127@users.noreply.github.com>
2019-03-08 14:43:00 -0800

    add threading

--- Lab5/Lab5Route.c
+++ Lab5/Lab5Route.c
@@ -6,8 +6,8 @@ Compile by: gcc -o lab5 Lab5Route.c -lpthread -std=C99
 Run by: ./lab5 {machine ID (0-3)} {num of machines} {cost file} {machines file}
 ex: ./lab5 0 4 cost machines
 
-1)router ID = argv[1]
-2)nUm of nodes = argv[2]
+1)router ID = argv[1]⟶ tells what node the program is being run
+2)num of nodes = argv[2]
 3)cost = argv[3]
 4)machine = argv[4]
 */
@@ -19,6 +19,9 @@ ex: ./lab5 0 4 cost machines
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h> //sleep
+#include <time.h> //random
+#include <pthread.h> //threads library
 
 /* define Machine struct */
 typedef struct{
@@ -31,17 +34,27 @@ typedef struct{
 /* Global Variable */
 int cost_matrix[numNodes][numNodes];
 Machine linux_machines[numNodes];
+int srcID;
 int sock;
 pthread_mutex_t lock;
 
 
 
-/* Link State Thread */
-void* linkState(void* a){
+/* Link State Thread - Thread 3 */
+void* linkState(void* args){
+    int srcNode;
+    int leastDistanceArray[numNodes];
+    bool visited[numNodes];
+    while true{
+        for(srcNode=0; srcNode<numNode; ++srcNode){
+            leastDistanceArray[sourceNode] =
+        }
+    }
+    sleep((rand()%10+10));
 
 }
 
-/* Recieve Info Thread */
+/* Recieve Info Thread - Thread 2 */
 void* recieveInfo(void* b){
 
 }
@@ -55,10 +68,18 @@ void printTable(void){
 }
 /* Main Function */
 int main(int argc, char *argv[]){
-    int i;
-    /* Fill cost matrix */{
+    /* setup threads */
+    pthread_t thr2, thr3;
+    pthread_create(&thr3, NULL, linkState, NULL);
+    pthread_create(&thr2, NULL, recieveInfo, NULL);
+    /* end thread setup */
+
+    srand(time(NULL));  //seed random
+    srcID = atoi(argv[1]);
+    /* Fill cost_matrix */{
     FILE* fp = fopen(argv[3], "r"); //open cost file for reading
 
+    int i;
     for(i=0; i<argv[2]; ++i){
         fscanf(fp, "%d %d %d %d", &cost_matrix[i][0], &cost_matrix[i][1], &cost_matrix[i][2], &cost_matrix[i][3]);
     }
@@ -68,10 +89,11 @@ int main(int argc, char *argv[]){
 
     /* Fill Machine structs */{
     fp = fopen(argv[4], "r");   //open machines file for reading
-    for(i=0; i<argv[2]; ++i){
-        fscanf(fp, "%s %s %d", linux_machines[i].)
+    for(i=0; i<numNodes; ++i){
+        fscanf(fp, "%s %s %d", linux_machines[i].name, linux_machines[i].ip, linux_machines[i].port)
     }
     }
+
 
 
 
